a6b38a02ea3ce70568e4834a5fcbae680e736744
ashabhi101 <abhilash362@gmail.com>
2019-08-29 23:26:47 -0500

    setup file for python 3, 3.4, 3.5 and 3.6

---
+++ setup.py
@@ -0,0 +1,28 @@
+!/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='dataset-shuft',
+    packages=[],
+    version='0.0.1',
+    description='dataset-shift for a Python package',
+    author='Abhilash Bokka',
+    license=Null,
+    author_email='abhilash.bokka@ucdenver.edu',
+    url='https://github.com/ashabhi101/dataset-shift',
+    keywords=['dataset', 'template', 'package','data shift', 'shift','drift', 'covariant shift'],
+    classifiers=[
+        'Development Status :: 1 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: Pending',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Software Development',
+    ],
+)
